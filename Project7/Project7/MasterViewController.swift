d01b2e3a6496e459fd69c1b2e729bb7d31e9acab
Devan Beitel <dbeitel@uabmc.edu>
2016-08-17 14:19:19 -0500

    Finishes Project 7 - Whitehouse Petitions. Code not fully working due to Swift 3.

---
+++ Project7/Project7/MasterViewController.swift
@@ -0,0 +1,107 @@
+//
+//  MasterViewController.swift
+//  Project7
+//
+//  Created by Devan Beitel on 8/16/16.
+//  Copyright © 2016 Devan Beitel. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [[String: String]]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&amp;limit=100"
+        }
+        
+        if let url = NSURL(string: urlString) {
+            if let data = try? NSData(contentsOf: url as URL, options: []) {
+                let json = JSON(data: data as Data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    parseJSON(json: json)
+                } else {
+                    showError()
+                }
+            } else {
+                showError()
+            }
+        } else {
+            showError()
+        }
+    }
+    
+    func parseJSON(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            objects.append(obj)
+        }
+        
+        tableView.reloadData()
+    }
+
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true, completion: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Segues
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row]
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object["title"]
+        cell.detailTextLabel?.text = object["body"]
+        return cell
+    }
+    
+    
+}
